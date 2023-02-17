23b0d570b1914b9a5fbbcf4195ecd5363326b6be
Sivakumar S <sivakumarit42@gmail.com>
2023-02-17 15:33:26 +0530

    Print in shell

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,27 @@
+code_dir=$(pwd)
+
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35mRemoving Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[35mDownloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[35mExtracting Downloaded Frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35mCopying Nginx Config for RoboShop\e[0m"
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35mStarting nginx\e[0m"
 systemctl restart nginx
 
-## Roboshop config is not copied
-## if any command is errored or failed, we need to stop the script
+## RoboShop Config is not copied
+## If any command is errored or failed, we need to stop the script
+
