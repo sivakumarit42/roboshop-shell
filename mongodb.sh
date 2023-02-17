aa83826e441d234ce54255c46a7e4e3d37eb3adc
Sivakumar S <sivakumarit42@gmail.com>
2023-02-17 17:20:25 +0530

    changes in directory

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,7 @@ code_dir=$(pwd)
 
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-/etc/nginx/default.d/roboshop.conf
+systemctl enable mongod
+systemctl start mongod
 
 #update /etc/mongod.conf from 127.0.0.1 to 0.0.0.0
