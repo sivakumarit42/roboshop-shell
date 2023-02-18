11b9e21e278f87cb9285be7f2744945d5d9c2fd5
Sivakumar S <sivakumarit42@gmail.com>
2023-02-18 16:58:16 +0530

    sed editor in mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,6 +6,9 @@ cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
+print_head "Update MongoDB lisiting Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
 
