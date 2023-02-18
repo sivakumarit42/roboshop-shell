cc9b6b84adc07d9950b4b11be2e14d43f0db9757
Sivakumar S <sivakumarit42@gmail.com>
2023-02-18 16:00:39 +0530

    function in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,54 @@
 code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+print_head(){
+  echo -e "\e[36m$1\e[0m"
+}
+
+print_head "Downloading nodejs repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "installing nodejs"
+yum install nodejs -y &>>${log_file}
+
+print_head "Creating robshop user"
 useradd roboshop
+
+print_head "Creating app directory"
 mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+print_head "Removing old content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading catalogue repository"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Extracting catalogue content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "installing nodejs dependices"
+npm install &>>${log_file}
 
-mongo --host mongodb.devopsb72.online </app/schema/catalogue.js
+print_head "Copy SystemD Service File"
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload deamon"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enabling Catalogue service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Enabling Catalogue service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copying mongodb.repo"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "installing mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "load schema"
+mongo --host mongodb.devopsb72.online </app/schema/catalogue.js &>>${log_file}
 
