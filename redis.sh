b12a9fcced9496d07b50122a1bd1240e3a910251
Sivakumar S <sivakumarit42@gmail.com>
2023-02-20 12:24:49 +0530

    changes in redis

--- redis.sh
+++ redis.sh
@@ -10,17 +10,17 @@ dnf module enable redis:remi-6.2 -y &>>${log_file}
 status_check $?
 
 print_head "installing redis"
-yum install redis -y
+yum install redis -y &>>${log_file}
 status_check $?
 
 print_head "updating listening address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 status_check $?
 
 print_head "enable redis"
-systemctl enable redis
+systemctl enable redis &>>${log_file}
 status_check $?
 
 print_head "Restart redis"
-systemctl restart redis
+systemctl restart redis &>>${log_file}
 status_check $?
