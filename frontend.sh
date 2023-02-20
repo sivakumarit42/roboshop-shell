13c720620c775a26b0d7e2c5a56e3f652b305438
Sivakumar S <sivakumarit42@gmail.com>
2023-02-20 10:10:48 +0530

    Conditions in frontend

--- frontend.sh
+++ frontend.sh
@@ -12,27 +12,32 @@ status_check $?
 
 print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
-
+status_check $?
 
 echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 echo -e "\e[35mStarting nginx\e[0m"
 systemctl restart nginx &>>${log_file}
-
+status_check $?
 
 ## If any command is errored or failed, we need to stop the script
 #status of a command need to be printed
