6ba984c431458cf8508f33fb07485fb2b4736797
Sivakumar S <sivakumarit42@gmail.com>
2023-02-20 10:28:09 +0530

    Conditions in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -50,6 +50,7 @@ print_head "Restart Catalogue service"
 systemctl restart catalogue &>>${log_file}
 status_check $?
 
+
 print_head "Copying mongodb.repo"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 
