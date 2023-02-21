bbc27ee5d45e15505d15e2c4f3b0045cf50f1f89
Sivakumar S <sivakumarit42@gmail.com>
2023-02-21 15:37:51 +0530

    changes in common.sh

--- mysql.sh
+++ mysql.sh
@@ -2,9 +2,9 @@ source common.sh
 
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo -e "\e[36m missing mysql root password argument\e[0m"
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
   exit 1
-  fi
+fi
 
 print_head "Disabling MySQL 8 Version"
 dnf module disable mysql -y  &>>${log_file}
@@ -27,11 +27,9 @@ systemctl start mysqld  &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-  echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
-  if [ $? -ne 0 ]; then
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
   mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
-  fi
-
+fi
 status_check $?
-
 
