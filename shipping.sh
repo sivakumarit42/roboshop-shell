9749c934c950a0294a736aae79aaf6935862bc86
Sivakumar S <sivakumarit42@gmail.com>
2023-02-21 17:55:23 +0530

    changes in common.sh

---
+++ shipping.sh
@@ -0,0 +1,13 @@
+source common.sh
+
+mysql_root_password=$1
+
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+  exit 1
+fi
+
+component=shipping
+schema_type="mysql"
+java
+
