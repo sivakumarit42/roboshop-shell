69e9ac44cca1c9ee9d6d4cbe42248baf3dfc00bf
Sivakumar S <sivakumarit42@gmail.com>
2023-02-21 15:04:00 +0530

    changes in common.sh

---
+++ configs/mysql.sh
@@ -0,0 +1,34 @@
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ];then
+  echo -e "\e[36m missing mysql root password argument\e[0m"
+  exit 1
+  fi
+
+print_head "Disabling MySQL 8 Version"
+dnf module disable mysql -y  &>>${log_file}
+status_check $?
+
+print_head "Copy MySQL repo file"
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo  &>>${log_file}
+status_check $?
+
+print_head "Installing MySQL Server"
+yum install mysql-community-server -y  &>>${log_file}
+status_check $?
+
+print_head "Enable MYSQL Service"
+systemctl enable mysqld  &>>${log_file}
+status_check $?
+
+print_head "Start MySQL Service"
+systemctl start mysqld  &>>${log_file}
+status_check $?
+
+print_head "Set Root Password"
+
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+
+status_check $?
+
