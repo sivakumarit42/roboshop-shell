dfd6533d561e020d83218cc7c9cff2c60822ddc5
Sivakumar S <sivakumarit42@gmail.com>
2023-02-21 12:37:49 +0530

    changes in common.sh

--- common.sh
+++ common.sh
@@ -5,3 +5,13 @@ rm -f ${log_file}
 print_head(){
   echo -e "\e[36m$1\e[0m"
 }
+
+status_check(){
+  if [ $1 -eq 0 ]
+     then
+       echo successfull
+       else
+         echo failure
+         echo "Read the log file ${log_file} for more information about error"
+         exit 1
+         fi
