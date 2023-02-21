631c2aa58a26530d6770a0dc15401ad658b6f0d8
Sivakumar S <sivakumarit42@gmail.com>
2023-02-21 15:32:44 +0530

    changes in common.sh

--- mysql.sh
+++ mysql.sh
@@ -27,8 +27,10 @@ systemctl start mysqld  &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-
+  echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+  if [ $? -ne 0 ];then
   mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+  fi
 
 status_check $?
 
